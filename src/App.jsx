a17bc58836f045192c8739af05f2eef8721b1e03
AhnEunJin <eunjin.an@datastreams.co.kr>
2024-07-03 11:57:03 +0900

    route setting

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,28 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import AppLayout from './layout/AppLayout';
+import HomePage from './pages/HomePage/HomePage';
+import MoviePage from './pages/Movies/MoviePage';
+import MovieDetailPage from './pages/MovieDetail/MovieDetailPage';
+import NotFoundPage from './pages/NotFoundPage/NotFoundPage';
 
 function App() {
-  return <div></div>;
+    return (
+        <div>
+            <Routes>
+                <Route path='/' element={<AppLayout />}>
+                    <Route index element={<HomePage />} />
+                    <Route path='/movies'>
+                        <Route index element={<MoviePage />} />
+                        <Route path=':id' element={<MovieDetailPage />} />
+                    </Route>
+                </Route>
+
+                {/* 위의 route를 제외한 path들 ... */}
+                <Route path='*' element={<NotFoundPage />} />
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
