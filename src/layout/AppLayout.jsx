e464a11f1e18e7f9ef6981d6a7eea83b9f1841cc
AhnEunJin <modic970515@gmail.com>
2024-09-02 01:56:50 +0900

    AppLayout

--- src/layout/AppLayout.jsx
+++ src/layout/AppLayout.jsx
@@ -1,5 +1,46 @@
 import React from 'react';
+import { Button, Container, Form, Image, Nav, Navbar } from 'react-bootstrap';
+import { Outlet } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
 
 export default function AppLayout() {
-  return <div>navbar</div>;
+  return (
+    <div>
+      <Navbar expand='lg' bg='black' data-bs-theme='dark'>
+        <Container fluid>
+          <Navbar.Brand href='/'>
+            <Image
+              width={50}
+              height={40}
+              src='https://www.liderlogo.es/wp-content/uploads/2022/12/pasted-image-0-6-1024x576.png'
+            />
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls='navbarScroll' />
+          <Navbar.Collapse id='navbarScroll'>
+            <Nav
+              className='me-auto my-2 my-lg-0'
+              style={{ maxHeight: '100px' }}
+            >
+              <Nav.Link href='/'>Home</Nav.Link>
+              <Nav.Link href='/movies'>Movies</Nav.Link>
+            </Nav>
+            <Form className='d-flex'>
+              <Form.Control
+                type='search'
+                placeholder='Search'
+                className='me-2'
+                aria-label='Search'
+              />
+
+              <Button variant='outline-danger'>
+                <FontAwesomeIcon icon={faSearch} />
+              </Button>
+            </Form>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      <Outlet />
+    </div>
+  );
 }
