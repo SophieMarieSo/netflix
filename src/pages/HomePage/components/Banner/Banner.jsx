e1004705ccf1c1469983bb7e3bb6ad0cc4c7498e
AhnEunJin <modic970515@gmail.com>
2024-09-02 12:28:53 +0900

    design Banner

--- src/pages/HomePage/components/Banner/Banner.jsx
+++ src/pages/HomePage/components/Banner/Banner.jsx
@@ -1,8 +1,33 @@
 import React from 'react';
 import { usePopularMoviesQuery } from '../../../../hooks/usePopularMovies';
+import { Alert, Spinner } from 'react-bootstrap';
+import './Banner.style.css';
 
 export default function Banner() {
-  const { data } = usePopularMoviesQuery();
+  const { data, isLoading, isError, error } = usePopularMoviesQuery();
 
-  return <div>Banner</div>;
+  if (isLoading) {
+    <Spinner animation='border' />;
+  }
+
+  if (isError) {
+    <Alert variant={'danger'}>{error.message}</Alert>;
+  }
+
+  return (
+    <div
+      style={{
+        backgroundImage:
+          'url(' +
+          `https://media.themoviedb.org/t/p/w533_and_h300_bestv2/${data?.results[0].poster_path}` +
+          ')',
+      }}
+      className='banner'
+    >
+      <div className='text-white desc'>
+        <h1>{data?.results[0].title}</h1>
+        <p>{data?.results[0].overview}</p>
+      </div>
+    </div>
+  );
 }
