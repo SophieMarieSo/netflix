f23ee28ad1ca7a7a826bde82a45836bc4760e0ce
AhnEunJin <modic970515@gmail.com>
2024-09-05 01:45:54 +0900

    pagination

--- src/pages/Movies/MoviesPage.jsx
+++ src/pages/Movies/MoviesPage.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+import ReactPaginate from 'react-paginate';
 import { Alert, Col, Container, Row, Spinner } from 'react-bootstrap';
 import { useSearchMovieQuery } from '../../hooks/useSearchMovie';
 import { useSearchParams } from 'react-router-dom';
@@ -9,8 +10,12 @@ import MovieCard from '../../common/MovieCard/MovieCard';
 // keyword 입력 -> keyword와 관련된 영화를 보여줌
 export default function MoviesPage() {
   const [query, setQuery] = useSearchParams();
+  const [page, setPage] = useState(1);
   const keyword = query.get('q');
-  const { data, isLoading, isError, error } = useSearchMovieQuery(keyword);
+  const { data, isLoading, isError, error } = useSearchMovieQuery(
+    keyword,
+    page
+  );
 
   if (isLoading) {
     <Spinner animation='border' />;
@@ -19,6 +24,10 @@ export default function MoviesPage() {
   if (isError) {
     <Alert variant={'danger'}>{error.message}</Alert>;
   }
+
+  const handlePageClick = ({ selected }) => {
+    setPage(selected + 1);
+  };
 
   return (
     <Container>
@@ -34,6 +43,27 @@ export default function MoviesPage() {
               </Col>
             ))}
           </Row>
+          <ReactPaginate
+            nextLabel='next >'
+            onPageChange={handlePageClick}
+            pageRangeDisplayed={3}
+            marginPagesDisplayed={2}
+            pageCount={data?.total_pages} // 전체페이지 수
+            previousLabel='< previous'
+            pageClassName='page-item'
+            pageLinkClassName='page-link'
+            previousClassName='page-item'
+            previousLinkClassName='page-link'
+            nextClassName='page-item'
+            nextLinkClassName='page-link'
+            breakLabel='...'
+            breakClassName='page-item'
+            breakLinkClassName='page-link'
+            containerClassName='pagination'
+            activeClassName='active'
+            renderOnZeroPageCount={null}
+            forcePage={page - 1}
+          />
         </Col>
       </Row>
     </Container>
